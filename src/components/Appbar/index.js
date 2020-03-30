d186b6ef33f3013020b1478ddcf57ad3e4e5b116
eloisa.fagundes <lola.fernanda@hotmail.com>
2020-03-30 18:57:41 -0300

    adiciona estilização da DetailPage e AppBar

--- src/components/Appbar/index.js
+++ src/components/Appbar/index.js
@@ -1,21 +1,75 @@
 import React, { Component } from "react";
-import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import { AppBar, Toolbar, Typography, Button, IconButton } from "@material-ui/core";
+import styled from 'styled-components'
+import { connect } from "react-redux";
+import { push } from "connected-react-router";
+import { routes } from '../../containers/Router'
+import { PowerSettingsNewRounded } from '@material-ui/icons';
 
+
+
+const ToolbarStyled = styled(Toolbar)`
+display: flex;
+justify-content: space-between;
+`
 
 class Appbar extends Component {
+
+
+  logout = () => {
+    this.props.goToLogin()
+  }
+
+
   render() {
+    const { goToFeed } = this.props
+    let buttonsPersonalized
+    switch (this.props.page) {
+      case "detail":
+        buttonsPersonalized =
+          <div>
+            <Button onClick={goToFeed} color="inherit">Feed</Button>
+            <IconButton color="inherit" onClick={this.logout}>
+              <PowerSettingsNewRounded />
+            </IconButton>
+          </div>
+        break;
+
+      case "feed":
+        buttonsPersonalized = <IconButton color="inherit" 
+        onClick={this.logout}>
+          <PowerSettingsNewRounded />
+        </IconButton>
+        break;
+
+      default:
+        buttonsPersonalized = ""
+        break;
+    }
+
+
+
     return (
       <div>
         <AppBar position="static">
-          <Toolbar variant="dense">
+          <ToolbarStyled variant="dense">
             <Typography variant="h6" color="inherit">
               4Reddit
           </Typography>
-          </Toolbar>
+            {buttonsPersonalized}
+          </ToolbarStyled>
         </AppBar>
       </div>
     );
   }
 }
 
-export default Appbar;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    goToFeed: () => dispatch(push(routes.feed)),
+    goToLogin: () => dispatch(push(routes.root))
+  }
+}
+
+
+export default connect(null, mapDispatchToProps)(Appbar);
