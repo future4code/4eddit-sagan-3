4ff4cadaa1f84e711a4b0d6d29177ce958e744ba
eloisa.fagundes <lola.fernanda@hotmail.com>
2020-03-30 14:21:48 -0300

    adiciona estilização da página de login

--- src/containers/LoginPage/index.js
+++ src/containers/LoginPage/index.js
@@ -1,19 +1,65 @@
 import React, { Component } from "react";
-import { Button } from "@material-ui/core";
+import { connect } from "react-redux";
+import { push } from "connected-react-router";
+import { routes } from '../Router'
+
 import Appbar from "../../components/Appbar";
+
+import styled from 'styled-components';
+import { TextField, Button } from "@material-ui/core";
+
+const LoginWrapper = styled.div`
+display: flex;
+flex-direction: column;
+margin: 2rem auto;
+width: 30vw;
+min-height: 80vh;
+justify-content: center;
+@media screen and (max-device-width: 1200px){
+ width: 90vw;
+}
+`
+const FormLogin = styled.form`
+display:flex;
+flex-direction: column;
+`
+const ButtonStyled = styled(Button)`
+margin: 1rem auto;
+width: 150px;
+
+`
 
 
 class LoginPage extends Component {
   render() {
+    const { goToLogin } = this.props
     return (
       <div>
         <Appbar />
-        <Button color="primary" variant="contained"> Clica em mim</Button>
-        <Button color="secondary" variant="contained"> Clica em mim de novo</Button>
-        LoginPage
+        <LoginWrapper>
+          <h1>Login</h1>
+
+          <FormLogin autoComplete="on">
+            <TextField id="email" label="E-mail" variant="outlined" margin="normal" />
+            <TextField id="senha" label="Senha" variant="outlined" margin="normal" />
+            <ButtonStyled type="submit" color="primary" variant="contained"> Entrar </ButtonStyled>
+          </FormLogin>
+
+          <ButtonStyled
+            onClick={goToLogin}
+            color="primary"
+            variant="contained">
+            Cadastrar </ButtonStyled>
+        </LoginWrapper>
       </div>
     );
   }
 }
 
-export default LoginPage;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    goToLogin: () => dispatch(push(routes.register))
+  }
+}
+
+export default connect(null, mapDispatchToProps)(LoginPage);
