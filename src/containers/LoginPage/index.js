1f8ff62bd2ba6f6c57ff9832e302565f447185d8
eloisa.fagundes <lola.fernanda@hotmail.com>
2020-03-30 12:09:25 -0300

    adiciona tema do 4reddit

--- src/containers/LoginPage/index.js
+++ src/containers/LoginPage/index.js
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
+import { Button } from "@material-ui/core";
 
 class LoginPage extends Component {
   render() {
     return (
       <div>
+        <Button color="primary" variant="contained"> Clica em mim</Button>
+        <Button color="secondary" variant="contained"> Clica em mim de novo</Button>
         LoginPage
       </div>
     );
--- src/style/theme.js
+++ src/style/theme.js
@@ -1,8 +1,5 @@
 import { createMuiTheme } from "@material-ui/core";
-import orange from "@material-ui/core/colors/orange";
 
-export default createMuiTheme({
-  palette: {
-    primary: orange
-  }
-});
+export default createMuiTheme(
+  {"palette":{"common":{"black":"#000","white":"#fff"},"background":{"paper":"#fff","default":"#fafafa"},"primary":{"light":"rgba(163, 215, 255, 1)","main":"rgba(0, 121, 211, 0.87)","dark":"rgba(23, 28, 153, 1)","contrastText":"#fff"},"secondary":{"light":"#ff4081","main":"#f50057","dark":"#c51162","contrastText":"#fff"},"error":{"light":"#e57373","main":"#f44336","dark":"#d32f2f","contrastText":"#fff"},"text":{"primary":"rgba(0, 0, 0, 0.87)","secondary":"rgba(0, 0, 0, 0.54)","disabled":"rgba(0, 0, 0, 0.38)","hint":"rgba(0, 0, 0, 0.38)"}}}
+);
